c6fd7179937ca3b9a747c00b83db224e20eca6bc
William Brownson <Williamiv95@gmail.com>
2015-03-17 00:51:14 -0700

    ReadME update

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ How to play:
   - You will need to install both the lastest version of Ruby, and the Gosu framework. ("gem install gosu")
   - run the game by either double clicking "BunnyInvaders.rb" or running "ruby BunnyInvaders.rb" in a terminal.
   - Control with the arrow keys, and use spacebar to shoot.
+
+  
+All images are from openclipart.org, a site that hosts open source images for public use.
+https://openclipart.org/share
+ 
+ 
+Space_Invader:
+https://openclipart.org/detail/216048/Bunny
+
+Circle:
+https://openclipart.org/detail/16074/Heart
+
+Space_Invaders_Ship:
+https://openclipart.org/detail/174020/Wolf%20space%20ship
---
+++ SpaceInvaders.rb
@@ -0,0 +1,239 @@
+#! usr/bin/env/ruby
+
+require 'gosu'
+
+$playerSpeed = 5
+$invaderSpeed = 2
+$laserSpeed = 10
+
+class GameWindow < Gosu::Window
+	
+	attr_accessor :game
+	
+	def initialize
+		super(480, 640, false)
+		self.caption = "Bunny Invaders"
+		@game = Game.new(self)
+	end
+	
+	def update
+		if @game.checkForEnd
+			self.close
+		end
+		
+		if button_down? Gosu::KbLeft then
+			@game.playerShip.velocity = -1
+		end
+		
+		if button_down? Gosu::KbRight then
+			@game.playerShip.velocity = 1
+		end
+		
+		if button_down? Gosu::KbSpace
+			if @game.laser == nil
+				@game.playerShip.fire
+			end
+		end
+		
+		@game.turnInvaders
+		@game.moveObjects
+		@game.checkCollisions
+		@game.playerShip.velocity = 0
+		@game.cleanUp
+	end
+	
+	def draw
+		@game.invaders.each { |invader| invader.draw() }
+		@game.playerShip.draw()
+		if @game.laser != nil
+			@game.laser.draw()
+		end
+	end
+
+end #GameWindow
+
+class Game
+
+	attr_accessor :invaders
+	attr_accessor :playerShip
+	attr_accessor :laser
+	
+	def initialize(window)
+		@window = window
+	
+		@invaders = Array.new
+		@playerShip = PlayerShip.new( 218 , 600 , @window )
+		@laser = nil
+		
+		spawnInvaders
+	end
+	
+	def spawnInvaders
+		for i in 1..4
+			@invaders << SpaceInvader.new( i * 100 - 32 , 0 , @window)
+		end
+	end
+	
+	def victory?
+		if @invaders.count == 0
+			puts("You Win!")
+			return true
+		end
+		return false
+	end
+	
+	def defeat?
+		if @invaders.last.y >= 600
+			puts("GAME OVER")
+			return true
+		end
+		return false
+	end
+	
+	def checkCollisions
+		if laser != nil
+			for invader in @invaders
+					if distanceBetween?(laser.x , laser.y , invader.x , invader.y) <= 50
+						@invaders.delete(invader)
+						@laser = nil
+						$invaderSpeed = $invaderSpeed + 2 #to simulate the original game's "speeding up" after killing each invader
+						break
+					end
+			end
+		end
+	end
+	
+	def distanceBetween?( x1 , y1 , x2 , y2 )
+		return ((x1 - x2)**2 + (y1 - y2)**2)**0.5 #simple distance formula
+	end
+	
+	def moveObjects
+		@invaders.each { |invader| invader.x += invader.velocity * $invaderSpeed }
+		@playerShip.x += playerShip.velocity * $playerSpeed
+		if @laser != nil
+			@laser.y -= @laser.velocity * $laserSpeed
+		end
+	end
+	
+	def turnInvaders
+		x = 0
+		for i in @invaders
+			@invaders[x].turnAround
+			x = x + 1
+		end
+	end
+	
+	def checkForEnd
+		
+		if victory? or defeat?
+			return true
+		else
+			return false
+		end
+	
+	end
+	
+	def cleanUp
+		if @laser != nil
+			if @laser.y <= 0
+				@laser = nil
+			end
+		end
+	end
+
+end #Game
+
+class SpaceObject
+	
+	attr_accessor :x
+	attr_accessor :y
+	attr_accessor :structure
+	attr_accessor :velocity
+	
+	def initialize( x , y , window)
+		@x = x
+		@y = y
+		@z = 1
+		@window = window
+		@velocity = 0
+	end
+	
+	def draw( xf , yf )
+		@structure.draw( @x , @y, @z , xf , yf )
+	end
+	
+end #SpaceObject
+
+class PlayerShip < SpaceObject
+		
+	def initialize ( x , y , window )
+		super	
+		@window = window
+		@structure = Gosu::Image.new(@window, "Space_Invaders_Ship.png", false)
+		@xf = 0.22
+		@yf = 0.22
+	end
+	
+	def draw()
+		super(@xf , @yf)
+	end
+	
+	def fire()
+		@window.game.laser = (Laser.new( @x + 18 , @y , @window ))
+	end
+	
+end #PlayerShip
+
+class SpaceInvader < SpaceObject
+
+	def initialize ( x , y , window )
+		super
+		@z = 3
+		@structure = Gosu::Image.new( window , "Space_Invader.png" , false )
+		@velocity = 1
+		@xf = 0.225
+		@yf = 0.225
+	end
+	
+	def draw()
+		super(@xf , @yf)
+	end
+	
+	def turnAround()
+		if @x > 422
+			@y = @y + 50
+			@velocity = -1
+			@x = 420
+		elsif @x < 0
+			@y = @y + 50
+			@velocity = 1
+			@x = 2
+		end
+	end
+	
+end #SpaceInvader
+
+class Laser < SpaceObject
+	
+	attr_accessor :x
+	attr_accessor :y
+
+	def initialize( x , y , window )
+		super
+		@z = 2
+		@structure = Gosu::Image.new( window , "Circle.png" , false )
+		@xf = 0.1
+		@yf = 0.1
+		@velocity = 1
+	end
+	
+	def draw()
+		super( @xf , @yf )
+	end
+	
+end
+
+######################################################################################
+
+$The_Game = GameWindow.new()
+$The_Game.show()
