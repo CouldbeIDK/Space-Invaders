0e1d8c6d05da10faeb2e3f6b29d2c58269410639
William Brownson <Williamiv95@gmail.com>
2015-03-17 10:36:10 -0700

    Fixed tests
    Separated the Classes from the code, and fixed the tests.

--- sitest.rb
+++ sitest.rb
@@ -1,17 +1,22 @@
-require_relative 'SpaceInvadersv2'
+require_relative 'BunnyInvadersClasses'
 require 'test/unit'
+
+
+class GameWindow < Gosu::Window
+
+end
 
 class Testing < Test::Unit::TestCase
 
 
 	def setup
-		@game = Game.new
+		@game = Game.new(GameWindow.new(2 , 2 , false))
 	end
 	def test_game
 		assert_equal(4, @game.invaders.count , "Error: spawnInvaders.")
 		assert_equal(false, @game.victory?, "Error: victory? false")
 		assert_equal(false, @game.defeat?, "Error: defeat? false")
-		@game.invaders[0].location = "bottom"
+		@game.invaders.last.y = 610
 		assert_equal(true, @game.defeat?, "Error: defeat? true")
 		for i in 0..3
 			@game.invaders.delete_at(0)
